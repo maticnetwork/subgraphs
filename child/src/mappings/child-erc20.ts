8f57267ee6b0956933c0d302956005d5c82e5ebf
Jaynti Kanani <jdkanani@gmail.com>
2020-10-04 00:18:38 +0530

    fix: format

--- child/src/mappings/child-erc20.ts
+++ child/src/mappings/child-erc20.ts
@@ -6,34 +6,34 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 // token, from, amount, to
 export function handleLogTransfer(event: LogTransfer): void {
-    let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-logTransfer")
+  let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-logTransfer")
 
-    transactionEntity.from = event.params.from
-    transactionEntity.to = event.params.to 
-    transactionEntity.value = toDecimal(event.params.amount, 18) 
-    transactionEntity.block = event.block.number
-    transactionEntity.timestamp = event.block.timestamp
-    transactionEntity.transaction = event.transaction.hash
-    transactionEntity.token = event.address
-    transactionEntity.type = "transfer"
+  transactionEntity.from = event.params.from
+  transactionEntity.to = event.params.to
+  transactionEntity.value = toDecimal(event.params.amount, 18)
+  transactionEntity.block = event.block.number
+  transactionEntity.timestamp = event.block.timestamp
+  transactionEntity.transaction = event.transaction.hash
+  transactionEntity.token = event.address
+  transactionEntity.type = "transfer"
 
-    if (transactionEntity.to.toHex() == ZERO_ADDRESS) {
-        return 
-    }
-    transactionEntity.save()
+  if (transactionEntity.to.toHex() == ZERO_ADDRESS) {
+    return
+  }
+  transactionEntity.save()
 }
 
 export function handleWithdraw(event: Withdraw): void {
-    let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-withdraw")
+  let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-withdraw")
 
-    transactionEntity.from = event.params.from
-    transactionEntity.to = null
-    transactionEntity.value = toDecimal(event.params.amount, 18) 
-    transactionEntity.block = event.block.number
-    transactionEntity.timestamp = event.block.timestamp
-    transactionEntity.transaction = event.transaction.hash
-    transactionEntity.token = event.address
-    transactionEntity.type = "withdraw"
+  transactionEntity.from = event.params.from
+  transactionEntity.to = null
+  transactionEntity.value = toDecimal(event.params.amount, 18)
+  transactionEntity.block = event.block.number
+  transactionEntity.timestamp = event.block.timestamp
+  transactionEntity.transaction = event.transaction.hash
+  transactionEntity.token = event.address
+  transactionEntity.type = "withdraw"
 
-    transactionEntity.save()
+  transactionEntity.save()
 }
