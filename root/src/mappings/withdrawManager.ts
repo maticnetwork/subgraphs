56efdba79c483f27c2d16c8baedd8092ff8ff1f9
Anjan Roy <anjanroy@yandex.com>
2020-11-20 13:13:42 +0530

    chg: handle exit started event

--- root/src/mappings/withdrawManager.ts
+++ root/src/mappings/withdrawManager.ts
@@ -1,37 +1,49 @@
-import { ExitStarted, ExitCancelled, ExitUpdated, Withdraw } from '../../generated/WithdrawManager/WithdrawManager'
-import { PlasmaExitStarted, PlasmaExitCancelled, PlasmaExitUpdated, PlasmaExitCompleted } from '../../generated/schema'
+import { ExitStarted, ExitCancelled, Withdraw } from '../../generated/WithdrawManager/WithdrawManager'
+import { PlasmaExit } from '../../generated/schema'
 
 
 export function handleExitStarted(event: ExitStarted): void {
-  let entity = new PlasmaExitStarted("exitstarted:" + event.params.exitId)
-  entity.exitor = event.params.exitor
+  let id = "plasma-exit-" + event.params.exitId
+  
+  // this is first time this entity being created i.e. during plasma
+  // exit start step
+  let entity = new PlasmaExit(id)
+  
   entity.exitId = event.params.exitId
+  entity.exitInitiator = event.params.exitor
   entity.token = event.params.token
   entity.amount = event.params.amount
   entity.isRegularExit = event.params.isRegularExit
+  entity.exited = 0
+  entity.exitStartedTxHash = event.transaction.hash
+  entity.exitStartedTimeStamp = event.block.timestamp
 
   // save entity
   entity.save()
 }
 
 export function handleExitCancelled(event: ExitCancelled): void {
-  let entity = new PlasmaExitCancelled("exitcancelled:" + event.params.exitId)
+  let id = "plasma-exit-" + event.params.exitId
+  
+  let entity = PlasmaExit.load(id)
+  if (entity == null) {
+    entity = new PlasmaExit(id)
+  }
+
   entity.exitId = event.params.exitId
 
   entity.save()
 }
 
-export function handleExitUpdated(event: ExitUpdated): void {
-  let entity = new PlasmaExitUpdated("exitupdated:" + event.params.exitId)
-  entity.exitId = event.params.exitId
-  entity.age = event.params.age
-  entity.signer = event.params.signer
-
-  entity.save()
-}
 
 export function handleWithdraw(event: Withdraw): void {
-  let entity = new PlasmaExitUpdated("exitcompleted:" + event.params.exitId)
+  let id = "plasma-exit-" + event.params.exitId
+  
+  let entity = PlasmaExit.load(id)
+  if (entity == null) {
+    entity = new PlasmaExit(id)
+  }
+
   entity.user = event.params.user
   entity.token = event.params.token
   entity.amount = event.params.amount
