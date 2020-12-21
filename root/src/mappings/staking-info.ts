5608e69fe62c7f329e32ad22df61b46752a9edf9
Jaynti Kanani <jdkanani@gmail.com>
2020-12-21 23:29:18 +0530

    new: add staking info subgraph

--- root/src/mappings/staking-info.ts
+++ root/src/mappings/staking-info.ts
@@ -1,229 +1,286 @@
-import { 
-  Staked, Unstaked, UnstakeInit, 
-  SignerChange, Restaked, Jailed, 
-  UnJailed, StakeUpdate, ClaimRewards, 
-  ConfirmAuction, ShareMinted, ShareBurned, 
-  UpdateCommissionRate, DelegatorUnstaked, DelegatorClaimedRewards,
+import { Delegator, Validator, StakingParams } from '../../generated/schema'
+import {
+  ClaimRewards,
+  DelegatorClaimedRewards,
+  DelegatorUnstaked,
+  DynastyValueChange,
+  Jailed,
+  OwnershipTransferred,
+  ProposerBonusChange,
+  Restaked,
+  ShareBurned,
+  ShareMinted,
+  SignerChange,
+  StakeUpdate,
+  Staked,
+  ThresholdChange,
+  UnJailed,
+  UnstakeInit,
+  Unstaked,
+  UpdateCommissionRate,
 } from '../../generated/StakingInfo/StakingInfo'
-import { Validator, Delegator } from '../../generated/schema'
 
 export function handleStaked(event: Staked): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = new Validator(id)
+  let entity = new Validator(id)
 
-    entity.validatorId = event.params.validatorId
-    entity.signer = event.params.signer
-    entity.activationEpoch = event.params.activationEpoch
-    entity.amount = event.params.amount
-    entity.total = event.params.total
-    entity.signerPubKey = event.params.signerPubKey
+  entity.validatorId = event.params.validatorId
+  entity.signer = event.params.signer
+  entity.activationEpoch = event.params.activationEpoch
+  entity.amount = event.params.amount
+  entity.total = event.params.total
+  entity.signerPubKey = event.params.signerPubkey
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleUnstaked(event: Unstaked): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.signer = event.params.user
-    entity.amount = event.params.amount
-    entity.total = event.params.total
+  entity.signer = event.params.user
+  entity.amount = event.params.amount
+  entity.total = event.params.total
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleUnstakeInit(event: UnstakeInit): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.signer = event.params.user
-    entity.deactivationEpoch = event.params.deactivationEpoch
-    entity.amount = event.params.amount
+  entity.signer = event.params.user
+  entity.deactivationEpoch = event.params.deactivationEpoch
+  entity.amount = event.params.amount
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleSignerChange(event: SignerChange): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.signer = event.params.newSigner
-    entity.signerPubKey = event.params.signerPubKey
+  entity.signer = event.params.newSigner
+  entity.signerPubKey = event.params.signerPubkey
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleRestaked(event: Restaked): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.amount = event.params.amount
-    entity.total = event.params.total
+  entity.amount = event.params.amount
+  entity.total = event.params.total
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleJailed(event: Jailed): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.jailEndEpoch = event.params.exitEpoch
-
-    // save entity
-    entity.save()
+  // save entity with jail end epoch
+  entity.jailEndEpoch = event.params.exitEpoch
+  entity.save()
 }
 
 export function handleUnJailed(event: UnJailed): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleStakeUpdate(event: StakeUpdate): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.amount = event.params.newAmount
+  entity.amount = event.params.newAmount
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleClaimRewards(event: ClaimRewards): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.amount = event.params.amount
-    entity.total = event.params.totalAmount
+  entity.amount = event.params.amount
+  entity.total = event.params.totalAmount
 
-    // save entity
-    entity.save()
-}
-
-export function handleConfirmAuction(event: ConfirmAuction): void {
-    let id = 'validator-' + event.params.validatorId
-
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
-
-    entity.validatorId = event.params.newValidatorId
-
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleShareMinted(event: ShareMinted): void {
-    let id = 'delegator-' + event.params.validatorId + event.params.user.toHexString()
+  const id = `delegator:${event.params.validatorId.toString()}:${event.params.user.toHexString()}`
 
-    let entity = Delegator.load(id)
-    if (entity == null) {
-      entity = new Delegator(id)
-    }
+  let entity = Delegator.load(id)
+  if (entity == null) {
+    entity = new Delegator(id)
+  }
 
-    entity.validatorId = event.params.validatorId
-    entity.address = event.params.user
-    entity.amount = entity.amount + event.params.amount
-    entity.tokens = event.params.tokens
+  entity.validatorId = event.params.validatorId
+  entity.address = event.params.user
+  entity.amount = entity.amount + event.params.amount
+  entity.tokens = event.params.tokens
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleShareBurned(event: ShareBurned): void {
-    let id = 'delegator-' + event.params.validatorId + event.params.user.toHexString()
+  const id = `delegator:${event.params.validatorId.toString()}:${event.params.user.toHexString()}`
 
-    let entity = Delegator.load(id)
-    if (entity == null) {
-      entity = new Delegator(id)
-    }
+  let entity = Delegator.load(id)
+  if (entity == null) {
+    entity = new Delegator(id)
+  }
 
-    entity.amount = entity.amount - event.params.amount
-    entity.tokens = event.params.tokens
+  entity.amount = entity.amount - event.params.amount
+  entity.tokens = event.params.tokens
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
 }
 
 export function handleDelegatorUnstaked(event: DelegatorUnstaked): void {
-    let id = 'delegator-' + event.params.validatorId + event.params.user.toHexString()
+  const id = `delegator:${event.params.validatorId.toString()}:${event.params.user.toHexString()}`
 
-    let entity = Delegator.load(id)
-    if (entity == null) {
-      entity = new Delegator(id)
-    }
+  let entity = Delegator.load(id)
+  if (entity == null) {
+    entity = new Delegator(id)
+  }
 
-    entity.amount = entity.amount - event.params.amount
-    
-    // save entity
-    entity.save()
+  entity.amount = entity.amount - event.params.amount
+
+  // save entity
+  entity.save()
 }
 
-export function handleDelegatorClaimedRewards(event: DelegatorClaimedRewards): void {
-    let id = 'delegator-' + event.params.validatorId + event.params.user.toHexString()
+export function handleDelegatorClaimedRewards(
+  event: DelegatorClaimedRewards
+): void {
+  const id = `delegator:${event.params.validatorId.toString()}:${event.params.user.toHexString()}`
 
-    let entity = Delegator.load(id)
-    if (entity == null) {
-      entity = new Delegator(id)
-    }
+  let entity = Delegator.load(id)
+  if (entity == null) {
+    entity = new Delegator(id)
+  }
 
-    entity.claimedRewards = entity.claimedRewards + event.params.rewards
-    
-    // save entity
-    entity.save()
+  entity.claimedRewards = entity.claimedRewards + event.params.rewards
+
+  // save entity
+  entity.save()
 }
 
 export function handleUpdateCommissionRate(event: UpdateCommissionRate): void {
-    let id = 'validator-' + event.params.validatorId
+  const id = `validator:${event.params.validatorId.toString()}`
 
-    let entity = Validator.load(id)
-    if (entity == null) {
-      entity = new Validator(id)
-    }
+  let entity = Validator.load(id)
+  if (entity == null) {
+    entity = new Validator(id)
+  }
 
-    entity.commissionRate = event.params.newCommissionRate
+  entity.commissionRate = event.params.newCommissionRate
 
-    // save entity
-    entity.save()
+  // save entity
+  entity.save()
+}
+
+//
+// Staking params handlers
+//
+
+export function handleDynastyValueChange(event: DynastyValueChange): void {
+  const id = `staking:params`
+  
+  let entity = StakingParams.load(id)
+  if (entity == null) {
+    entity = new StakingParams(id)
+  }
+
+  // save entity with dynasty
+  entity.dynasty = event.params.newDynasty
+  entity.save()
+}
+
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  const id = `staking:params`
+  
+  let entity = StakingParams.load(id)
+  if (entity == null) {
+    entity = new StakingParams(id)
+  }
+
+  // save entity with owner
+  entity.owner = event.params.newOwner
+  entity.save()
+}
+
+export function handleProposerBonusChange(event: ProposerBonusChange): void {
+  const id = `staking:params`
+  
+  let entity = StakingParams.load(id)
+  if (entity == null) {
+    entity = new StakingParams(id)
+  }
+
+  // save entity with proposer bonus
+  entity.proposerBonus = event.params.newProposerBonus
+  entity.save()
+}
+
+export function handleThresholdChange(event: ThresholdChange): void {
+  const id = `staking:params`
+  
+  let entity = StakingParams.load(id)
+  if (entity == null) {
+    entity = new StakingParams(id)
+  }
+
+  // save entity with validator threshold
+  entity.validatorThreshold = event.params.newThreshold
+  entity.save()
 }
