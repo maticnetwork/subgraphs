99635d1946f721c3701f6771df514b5b846ed3cb
Anjan Roy <anjanroy@yandex.com>
2021-02-01 21:46:20 +0530

    chg: using revert handling capable function for statesync data decoding

--- root/src/mappings/state-sync.ts
+++ root/src/mappings/state-sync.ts
@@ -12,15 +12,26 @@ export function handleStateSynced(event: StateSynced): void {
   entity.contract = event.params.contractAddress
 
   let decoder = Decoder.bind(Address.fromString(decoderAddress))
-  let decoded = decoder.decodeStateSyncData(event.params.data)
+  let callResult = decoder.try_decodeStateSyncData(event.params.data)
+
+  entity.transactionHash = event.transaction.hash
+  entity.timestamp = event.block.timestamp
+
+  if (callResult.reverted) {
+
+    entity.syncType = ""
+    entity.depositor_or_rootToken = ""
+    entity.depositedToken_or_childToken = ""
+    entity.data = event.params.data
+
+    return
+
+  }
 
   entity.syncType = decoded.value0
   entity.depositor_or_rootToken = decoded.value1
   entity.depositedToken_or_childToken = decoded.value2
   entity.data = decoded.value3
-
-  entity.transactionHash = event.transaction.hash
-  entity.timestamp = event.block.timestamp
 
   // save entity
   entity.save()
@@ -28,7 +39,7 @@ export function handleStateSynced(event: StateSynced): void {
 
 export function handleNewRegistration(event: NewRegistration): void {
   let id = 'registration:' + event.params.receiver.toHexString()
-  
+
   let entity = StateRegistration.load(id)
   if (entity == null) {
     entity = new StateRegistration(id)
@@ -43,7 +54,7 @@ export function handleNewRegistration(event: NewRegistration): void {
 
 export function handleRegistrationUpdated(event: RegistrationUpdated): void {
   let id = 'registration:' + event.params.receiver.toHexString()
-  
+
   let entity = StateRegistration.load(id)
   if (entity == null) {
     entity = new StateRegistration(id)
