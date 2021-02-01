15d488a3ba1c440a233a29a2978b16ec2c174a8e
Anjan Roy <anjanroy@yandex.com>
2021-02-01 20:35:10 +0530

    new: getting decoder contract address in template file for network addresses

--- root/src/network.template.ts
+++ root/src/network.template.ts
@@ -1,3 +1,4 @@
 export const network: string = '{{ network }}'
 export const stakingNftAddress: string = '{{ contracts.stakingNft.address }}'
 export const registryAddress: string = '{{ contracts.registry.address }}'
+export const decoderAddress: string = '{{ contracts.decoder.address }}'
