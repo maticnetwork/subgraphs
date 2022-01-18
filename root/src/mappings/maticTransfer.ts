3f873aee7df33b1b0d9aabf6dbf20a913dedd56e
nitinmittal23 <nitinmittal44066@gmail.com>
2022-01-18 15:23:39 +0530

    new: fetch matic transfers

---
+++ root/src/mappings/maticTransfer.ts
@@ -0,0 +1,16 @@
+import { Transfer } from '../../generated/TestToken/TestToken'
+import { MaticTransfer } from '../../generated/schema'
+
+export function handleTransfer(event: Transfer): void {  
+  let entity = new MaticTransfer(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+
+  entity.from = event.params.from
+  entity.to = null
+  entity.value = event.params.value
+  entity.block = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+  entity.token = event.address
+
+  entity.save()
+}
