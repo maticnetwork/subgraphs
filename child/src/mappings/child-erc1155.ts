812be397584162563351a5f7b7e92cf5fbfc1338
vibhurajeev <vibhurajeev@gmail.com>
2022-02-22 10:24:00 +0530

    new: burn indexing

---
+++ child/src/mappings/child-erc1155.ts
@@ -0,0 +1,73 @@
+import { BigInt } from '@graphprotocol/graph-ts'
+import { TransferBatch, TransferSingle } from '../../generated/ChildERC1155/ChildERC1155'
+import { TransactionEntity, GlobalTransferCounter } from '../../generated/schema'
+//import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { toDecimal } from '../helpers/numbers'
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+function getGlobalTransferCounter(): GlobalTransferCounter {
+    // Only one entry will be kept in this entity
+    let id = 'global-transfer-counter'
+    let entity = GlobalTransferCounter.load(id)
+    if (entity === null) {
+        entity = new GlobalTransferCounter(id)
+        entity.current = BigInt.fromI32(0)
+    }
+    return entity as GlobalTransferCounter
+}
+
+export function handleSingleTransfer(event: TransferSingle): void {
+
+    // Try to get what's current global plasma counter's state
+    // when called for very first time, it'll be `0`
+    let counter = getGlobalTransferCounter()
+    let updated = counter.current.plus(BigInt.fromI32(1))
+
+    // Updating global counter's state
+    counter.current = updated
+    counter.save()
+
+    const isWithdraw = event.params.to.toHex() === ZERO_ADDRESS || event.params.to === event.address ? true : false
+
+    let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + (isWithdraw ? '-withdraw' : '-transfer'))
+    transactionEntity.type = isWithdraw ? 'withdraw' : 'transfer'
+    transactionEntity.from = event.params.from
+    transactionEntity.to = event.params.to
+    transactionEntity.amount = event.params.value
+    transactionEntity.tokenId = event.params.id
+    transactionEntity.block = event.block.number
+    transactionEntity.timestamp = event.block.timestamp
+    transactionEntity.transaction = event.transaction.hash
+    transactionEntity.token = event.address
+    transactionEntity.tokenType = "ERC1155"
+    transactionEntity.isPos = true
+    transactionEntity.save()
+}
+
+export function handleBatchTransfer(event: TransferBatch): void {
+    // Try to get what's current global plasma counter's state
+    // when called for very first time, it'll be `0`
+    let counter = getGlobalTransferCounter()
+    let updated = counter.current.plus(BigInt.fromI32(1))
+
+    // Updating global counter's state
+    counter.current = updated
+    counter.save()
+
+    const isWithdraw = event.params.to.toHex() === ZERO_ADDRESS || event.params.to === event.address ? true : false
+
+    let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + (isWithdraw ? '-withdraw' : '-transfer'))
+
+    transactionEntity.type = isWithdraw ? 'withdraw' : 'transfer'
+    transactionEntity.from = event.params.from
+    transactionEntity.to = event.params.to
+    transactionEntity.amounts = event.params.values
+    transactionEntity.tokenIds = event.params.ids
+    transactionEntity.timestamp = event.block.timestamp
+    transactionEntity.transaction = event.transaction.hash
+    transactionEntity.token = event.address
+    transactionEntity.tokenType = "ERC1155"
+    transactionEntity.isPos = true
+    transactionEntity.save()
+}
--- child/src/mappings/child-erc20.ts
+++ child/src/mappings/child-erc20.ts
@@ -1,57 +1,88 @@
+import { BigInt, Address } from '@graphprotocol/graph-ts'
 import { LogTransfer, Withdraw, Transfer } from '../../generated/ChildERC20/ChildERC20'
-import { TransactionEntity } from '../../generated/schema'
+import { TransactionEntity, GlobalTransferCounter } from '../../generated/schema'
 import { toDecimal } from '../helpers/numbers'
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
+function getGlobalTransferCounter(): GlobalTransferCounter {
+  // Only one entry will be kept in this entity
+  let id = 'global-transfer-counter'
+  let entity = GlobalTransferCounter.load(id)
+  if (entity === null) {
+    entity = new GlobalTransferCounter(id)
+    entity.current = BigInt.fromI32(0)
+  }
+  return entity as GlobalTransferCounter
+}
+
 // token, from, amount, to
 export function handleLogTransfer(event: LogTransfer): void {
   let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + '-logTransfer')
-
   transactionEntity.from = event.params.from
   transactionEntity.to = event.params.to
-  transactionEntity.value = toDecimal(event.params.amount, 18)
+  transactionEntity.amount = event.params.amount
   transactionEntity.block = event.block.number
   transactionEntity.timestamp = event.block.timestamp
   transactionEntity.transaction = event.transaction.hash
   transactionEntity.token = event.address
   transactionEntity.type = 'transfer'
 
-  if (transactionEntity.to.toHex() == ZERO_ADDRESS) {
+  if (transactionEntity.to.toHex() === ZERO_ADDRESS) {
     return
   }
   transactionEntity.save()
 }
 
 export function handleWithdraw(event: Withdraw): void {
-  let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + '-withdraw')
 
+   // Try to get what's current global plasma counter's state
+  // when called for very first time, it'll be `0`
+  let counter = getGlobalTransferCounter()
+  let updated = counter.current.plus(BigInt.fromI32(1))
+
+  // Updating global counter's state
+  counter.current = updated
+  counter.save()
+
+  // Transaction Entity part
+  let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + '-withdraw')
   transactionEntity.from = event.params.from
-  transactionEntity.to = null
-  transactionEntity.value = toDecimal(event.params.amount, 18)
+  transactionEntity.to = Address.fromString(ZERO_ADDRESS)
+  transactionEntity.amount = event.params.amount
   transactionEntity.block = event.block.number
   transactionEntity.timestamp = event.block.timestamp
   transactionEntity.transaction = event.transaction.hash
   transactionEntity.token = event.address
   transactionEntity.type = 'withdraw'
+  transactionEntity.tokenType = "ERC20"
   transactionEntity.isPos = false
   transactionEntity.save()
 }
 
-export function handleBurnTransfer(event: Transfer): void {
+export function handleTransfer(event: Transfer): void {
 
-  if (event.params.to.toHex() == ZERO_ADDRESS) {
-    let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-withdraw')
+  // Try to get what's current global transfer counter's state
+  // when called for very first time, it'll be `0`
+  let counter = getGlobalTransferCounter()
+  let updated = counter.current.plus(BigInt.fromI32(1))
 
-    transactionEntity.from = event.params.from
-    transactionEntity.to = event.params.to
-    transactionEntity.value = toDecimal(event.params.value, 18)
-    transactionEntity.block = event.block.number
-    transactionEntity.timestamp = event.block.timestamp
-    transactionEntity.transaction = event.transaction.hash
-    transactionEntity.token = event.address
-    transactionEntity.type = 'withdraw'
-    transactionEntity.isPos = true
-    transactionEntity.save()
-  }
+  // Updating global counter's state
+  counter.current = updated
+  counter.save()
+
+  const isWithdraw = event.params.to.toHex() === ZERO_ADDRESS || event.params.to === event.address ? true : false
+  
+  let transactionEntity = new TransactionEntity(event.transaction.hash.toHex() + '-' + event.logIndex.toString() + (isWithdraw ? '-withdraw' : '-transfer'))
+  transactionEntity.type = isWithdraw ? 'withdraw' : 'transfer'
+  transactionEntity.from = event.params.from
+  transactionEntity.to = event.params.to
+  transactionEntity.amount = event.params.value
+  transactionEntity.block = event.block.number
+  transactionEntity.timestamp = event.block.timestamp
+  transactionEntity.transaction = event.transaction.hash
+  transactionEntity.token = event.address
+  transactionEntity.tokenType = "ERC20"
+  transactionEntity.isPos = true
+  transactionEntity.save()
 }
